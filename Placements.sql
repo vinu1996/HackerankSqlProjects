143c0384ae7325e87fa3d44bce3f5c435c28b20e
vinu1996 <36875903+vinu1996@users.noreply.github.com>
2024-04-22 00:39:32 +0530

    Create Placements.sql

---
+++ Placements.sql
@@ -0,0 +1,9 @@
+select s.name
+from students s 
+inner join friends f 
+on s.id=f.id 
+inner join packages p 
+on s.id=p.id inner join packages p2 
+on f.friend_id=p2.id 
+where p2.salary > p.salary 
+order by p2.salary asc;
